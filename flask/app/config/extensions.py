bbb660aaf6edd598666f38d6644493a6ee633393
Lawrence Ofoli Mensah <48308365+Ofoli@users.noreply.github.com>
2024-08-05 01:05:28 +0000

    Models (#2)
    * setup docker with compose
    
    * setup flask app
    
    * setup flask app
    
    * setup flask app
    
    * setup flask app
    
    * setup flask app
    
    * add auth-user model
    
    * add postpaid client models
    
    * add postpaid client models

--- flask/app/config/extensions.py
+++ flask/app/config/extensions.py
@@ -3,17 +3,22 @@ from flask import Flask
 from redis import Redis
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+flask_bcrypt = Bcrypt()
 
 
 def _init_redis(app: Flask) -> None:
-    app.extensions["redis"] = Redis(host=app.config["REDIS_HOST"], db=1, decode_responses=True)
+    app.extensions["redis"] = Redis(
+        host=app.config["REDIS_HOST"], db=1, decode_responses=True
+    )
 
 
 def init_extensions(app: Flask) -> None:
     db.init_app(app)
     migrate.init_app(app, db)
+    flask_bcrypt.init_app(app)
     _init_redis(app)
---
+++ flask/app/core/models/user.py
@@ -0,0 +1,71 @@
+import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Float, String, Integer
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+
+from ...config.extensions import flask_bcrypt
+
+
+class TimestampsMixin(object):
+    @declared_attr
+    def created_at(cls):
+        return Column(
+            DateTime, default=datetime.datetime.now(datetime.UTC), nullable=False
+        )
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(
+            DateTime,
+            default=datetime.datetime.now(datetime.UTC),
+            onupdate=datetime.datetime.now(datetime.UTC),
+            nullable=False,
+        )
+
+
+Base = declarative_base(cls=TimestampsMixin)
+
+
+class AuthUser(Base):
+    __tablename__ = "auth_user"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(100))
+    fullname = Column(String(255), nullable=False)
+
+    @password.setter
+    def set_password(self, password):
+        self.password = flask_bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.email)
+
+
+class BasePostpaidClient(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(255), nullable=False, unique=True)
+    rate = Column(Float, nullable=False)
+
+
+class ApiClient(BasePostpaidClient):
+    __tablename__ = "api_client"
+
+    aid = Column(Integer, nullable=False)
+
+
+class BlastClient(BasePostpaidClient):
+    __tablename__ = "blast_client"
+
+    user_id = Column(Integer, nullable=False)
+    reseller_id = Column(Integer, nullable=False)
+    is_reseller = Column(Boolean, default=False)
+
+
+class ESMEClient(BasePostpaidClient):
+    __tablename__ = "esme_client"
