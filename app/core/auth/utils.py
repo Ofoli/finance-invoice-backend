9694bbbafed47177b1725f0c91d86a555240b6c0
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-08-07 01:45:11 +0000

    add login endpoint

--- app/core/auth/utils.py
+++ app/core/auth/utils.py
@@ -4,7 +4,6 @@ from http import HTTPStatus
 from app.core.constants import APP_LOGGER
 from ..users.queries import get_user_by_email
 from ..utils.response import Response
-from ..schemas.user import AuthUserSchema
 
 logger = logging.getLogger(APP_LOGGER)
 
@@ -17,11 +16,15 @@ class Auth:
             user = get_user_by_email(email=email)
             if user is not None and user.check_password(password):
                 auth_token = user.generate_auth_token()
-                response = dict(user=AuthUserSchema().dump(
-                    user), token=auth_token)
+                response = dict(
+                    id=user.id,
+                    email=user.email,
+                    fullname=user.fullname,
+                    token=auth_token
+                )
                 return Response().success(response)
             else:
-                response = 'email or password does not match.'
+                response = 'Invalid email or password'
                 return Response(HTTPStatus.PRECONDITION_FAILED).failed(response)
 
         except Exception as e:
