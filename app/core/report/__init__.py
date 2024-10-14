5e6d7712c993f5386cd8ed5630631fb06264d800
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-10-14 19:28:59 +0000

    update report urls

--- app/core/report/__init__.py
+++ app/core/report/__init__.py
@@ -3,8 +3,15 @@ from flask_restful import Api
 
 # from .tasks import initiate_s3_fetch_script
 from .views.callbacks import S3ReportCallbackView, EmailReportCallbackView, EtzReportCallbackView
-from .views.query import MonthlyReportView
-from .constants import S3_REPORT_CALLBACK_URL, EMAIL_REPORT_CALLBACK_URL, ETZ_REPORT_CALLBACK_URL, MONTHLY_REPORT_URL
+from .views.query import MonthlyReportView, TopFiveStatsView, PastYearReportStats
+from .constants import (
+    S3_REPORT_CALLBACK_URL,
+    EMAIL_REPORT_CALLBACK_URL,
+    ETZ_REPORT_CALLBACK_URL,
+    MONTHLY_REPORT_URL,
+    TOP5_CLIENTS_STATS,
+    YEARLY_SERVICE_STATS,
+)
 
 report_bp = Blueprint("report", __name__)
 api = Api(report_bp)
@@ -13,3 +20,5 @@ api.add_resource(S3ReportCallbackView, S3_REPORT_CALLBACK_URL)
 api.add_resource(EmailReportCallbackView, EMAIL_REPORT_CALLBACK_URL)
 api.add_resource(EtzReportCallbackView, ETZ_REPORT_CALLBACK_URL)
 api.add_resource(MonthlyReportView, MONTHLY_REPORT_URL)
+api.add_resource(TopFiveStatsView, TOP5_CLIENTS_STATS)
+api.add_resource(PastYearReportStats, YEARLY_SERVICE_STATS)
--- app/core/report/constants.py
+++ app/core/report/constants.py
@@ -1,19 +1,23 @@
 import os
 from app.core.constants import FILES_DIR
 
+# FILES
+ETZ_SENT_FILES_DIR = os.path.join(FILES_DIR, "etz", "sent_files")
+ETZ_STAT_FILES_DIR = os.path.join(FILES_DIR, "etz", "stat_files")
+ETZ_REPORT_FILES_DIR = os.path.join(FILES_DIR, "etz", "reports")
+
 # URLS
-_S3_BASE_URL = "http://5.9.79.123/eomreport/invoice/api"
-_s7_BASE_URL = "https://api.nalosolutions.com:8888/lmensah"
+_s3_BASE_URL = os.environ.get("S3_BASE_URL", "")
+_s7_BASE_URL = os.environ.get("S7_BASE_URL", "")
+_s9_BASE_URL = os.environ.get("S9_BASE_URL", "")
 
-INITIATE_FETCH_URL = f"{_S3_BASE_URL}/initiate-fetch.php"
-INITIATE_ETZ_URL = f"{_S3_BASE_URL}/initiate-etz-report.php"
-GET_RESELLER_USERS_URL = f"{_S3_BASE_URL}/get-reseller-users.php"
-GET_USER_REPORT_URL = f"{_S3_BASE_URL}/report.php"
-GET_S9_USER_REPORT = "http://95.217.203.30:6610/reports/network-report"
-
-
+INITIATE_FETCH_URL = f"{_s3_BASE_URL}/initiate-fetch.php"
+INITIATE_ETZ_URL = f"{_s3_BASE_URL}/initiate-etz-report.php"
+GET_RESELLER_USERS_URL = f"{_s3_BASE_URL}/get-reseller-users.php"
+GET_USER_REPORT_URL = f"{_s3_BASE_URL}/report.php"
 GET_BLASTS_URL = f"{_s7_BASE_URL}/blast"
 GET_USER_RATE_URL = f"{_s7_BASE_URL}/fetch-rates.php/"
+GET_S9_USER_REPORT = f"{_s9_BASE_URL}/network-report"
 
 
 # ENDPOINTS
@@ -21,41 +25,8 @@ S3_REPORT_CALLBACK_URL = "/s3-callback"
 ETZ_REPORT_CALLBACK_URL = "/etz-callback"
 EMAIL_REPORT_CALLBACK_URL = "/email-report-callback"
 MONTHLY_REPORT_URL = "/monthly-report"
-
-
-# VALUES
-S3_CLIENT_AID = "s3000000"
-EMAIL_CLIENT_AID = "00000000"
-DEFAULT_RATE = 0.032
-DEFAULT_EMAIL_RATE = 0.04
-NETWORKS = {
-    "GLO": "23323",
-    "EXPRESS0": "23328",
-    "VODAFONE": "23350|23320",
-    "AIRTELTIGO": "23327|23357|23326|23356",
-    "MTN": "23324|23325|23359|23355|23354|23353",
-}
-ESME_SPACE_ROW = {
-    "account": "",
-    "network": "",
-    "count": "",
-}
-ALERTS_SPACE_ROW = {**ESME_SPACE_ROW, "page_count": ""}
-ETZ_SENT_FILES_DIR = os.path.join(FILES_DIR, "etz", "sent_files")
-ETZ_STAT_FILES_DIR = os.path.join(FILES_DIR, "etz", "stat_files")
-ETZ_REPORT_FILES_DIR = os.path.join(FILES_DIR, "etz", "reports")
-GET_USER_REPORT_URL = f"{_S3_BASE_URL}/report.php"
-GET_S9_USER_REPORT = "http://95.217.203.30:6610/reports/network-report"
-
-
-GET_BLASTS_URL = f"{_s7_BASE_URL}/blast"
-GET_USER_RATE_URL = f"{_s7_BASE_URL}/fetch-rates.php/"
-
-
-# ENDPOINTS
-S3_REPORT_CALLBACK_URL = "/s3-callback"
-ETZ_REPORT_CALLBACK_URL = "/etz-callback"
-EMAIL_REPORT_CALLBACK_URL = "/email-report-callback"
+TOP5_CLIENTS_STATS = "/top-five-clients"
+YEARLY_SERVICE_STATS = "/yearly-service-counts"
 
 
 # VALUES
