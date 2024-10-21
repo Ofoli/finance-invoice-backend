186d640112951625a1c109e42e477f88b9f5ce4f
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-10-21 19:43:21 +0000

    fix celery delay call error

--- app/core/report/views/callbacks.py
+++ app/core/report/views/callbacks.py
@@ -1,35 +1,29 @@
 from flask import request
 from marshmallow import ValidationError
 from http import HTTPStatus
-from typing import Callable
 from werkzeug.datastructures.file_storage import FileStorage
 
 
 from app.core.utils.auth import IsIPWhitelistedMixin
 from app.core.utils.http import Response
 from app.core.schemas.report import EmailReportCallbackDataSchema
-from ..tasks import handle_s3_report_callback, handle_email_report_callback, handle_etz_report_callback
-from ..utils.etz import save_etz_file
-
-
-def handle_post_callback(schema: Callable, task_method: Callable):
-    try:
-        validated_data = schema().load(request.get_json())
-        task = task_method(validated_data).delay()
-        return task.id
-    except ValidationError as err:
-        return Response(HTTPStatus.BAD_REQUEST).failed(err.messages)
+from app.core.report.tasks import (
+    handle_s3_report_callback,
+    handle_email_report_callback,
+    handle_etz_report_callback,
+)
+from app.core.report.utils.etz import save_etz_file
 
 
 class S3ReportCallbackView(IsIPWhitelistedMixin):
     def get(self):
-        task = handle_s3_report_callback().delay()
+        task = handle_s3_report_callback.delay()  # type: ignore
         return Response().success(task.id)
 
 
 class EtzReportCallbackView(IsIPWhitelistedMixin):
     def get(self):
-        task = handle_etz_report_callback().delay()
+        task = handle_etz_report_callback.delay()  # type: ignore
         return Response().success(task.id)
 
 
@@ -47,4 +41,9 @@ class EmailReportCallbackView(IsIPWhitelistedMixin):
     __schema = EmailReportCallbackDataSchema
 
     def post(self):
-        return handle_post_callback(self.__schema, handle_email_report_callback)
+        try:
+            validated_data = self.__schema().load(request.get_json())
+            task = handle_email_report_callback.delay(validated_data)  # type: ignore
+            return task.id
+        except ValidationError as err:
+            return Response(HTTPStatus.BAD_REQUEST).failed(err.messages)
